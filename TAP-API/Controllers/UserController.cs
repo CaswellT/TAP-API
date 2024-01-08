22cc41d627f31b758d7041ac587201612ca2a823
Caswell Taylor <caswell.taylor@ttu.edu>
2024-01-08 16:52:35 -0600

    Add project files.

---
+++ TAP-API/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TAP_API.Models;
+
+namespace TAP_API.Controllers
+{
+    [ApiController, Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet, Route("Get-Message")]
+        public IActionResult GetMessage()
+        {
+            UserInfo user = new UserInfo { FirstName = "John", LastName = "Eads" };
+
+
+
+            return Ok($"{user.FirstName} {user.LastName} can now use .net 8");
+        }
+    }
+}
---
+++ TAP-API/Models/UserInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace TAP_API.Models
+{
+    public class UserInfo
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
